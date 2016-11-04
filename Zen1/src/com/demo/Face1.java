a2c945317e9cd842311c10e51f388820e1d92343
Nipjoti <Nipjoti@192.168.0.5>
2016-11-04 20:00:06 +0530

    Change made in face1

--- Zen1/src/com/demo/Face1.java
+++ Zen1/src/com/demo/Face1.java
@@ -27,13 +27,5 @@ public class Face1 {
 		System.out.println(Title + "  Hey Mark I m on Facebook");
 		driver.close();
 		}
-	@Test
-	public void run2(){
-		WebDriver driver=new FirefoxDriver();
-		driver.get("https://accounts.google.com");
-		String A=driver.findElement(By.cssSelector(".logo.logo-w")).getAttribute("aria-label");
-	
-		System.out.println(A);
-		driver.close();
-		}
+
 }
